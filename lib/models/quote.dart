b11edb6aeae82f794ce077164fb06cc35ffd5346
Moamen Ibrahim <mmibrahem76@gmail.com>
2020-04-05 18:30:46 +0300

    Fix flutter analyse problem with equatable

--- lib/models/quote.dart
+++ lib/models/quote.dart
@@ -1,12 +1,9 @@
-import 'package:equatable/equatable.dart';
-
 class Quote {
   final id;
   final String quoteText;
   final String quoteAuthor;
   const Quote({this.id, this.quoteText, this.quoteAuthor});
 
-  @override
   List<Object> get props => [id, quoteText, quoteAuthor];
 
   static Quote fromJson(dynamic json) {
